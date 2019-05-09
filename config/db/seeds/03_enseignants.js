7837f21796348849aef250fda413cca026ccbdfe
saidiKhaled <khaledsd31@gmail.com>
2019-05-09 18:37:11 +0100

    pgcommit

--- config/db/seeds/03_enseignants.js
+++ config/db/seeds/03_enseignants.js
@@ -2,10 +2,16 @@ exports.seed = function (knex, Promise) {
 	// Deletes ALL existing entries
 	return knex('enseignants').del().then(function () {
 		// Inserts seed entries
-		return knex('enseignants').insert([
-			{ enseignant_id: 1, etablissement_id: 1, user_id: 1 },
-			{ enseignant_id: 2, etablissement_id: 1, user_id: 2 },
-			{ enseignant_id: 3, etablissement_id: 1, user_id: 3 }
+		return knex('enseignants').insert([{
+				enseignant_id: 1,
+				etablissement_id: 1,
+				user_id: 1
+			},
+			{
+				enseignant_id: 2,
+				etablissement_id: 1,
+				user_id: 2
+			}
 		]);
 	});
 };
--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,38 @@
 // Update with your config settings.
 module.exports = {
-	development : {
-		client     : 'mysql',
-		connection : {
-			host     : 'localhost',
-			user     : 'khaled',
-			password : 'Teenwolfs1',
-			database : 'plateforme_db',
-			charset  : 'utf8'
+	development: {
+		client: 'pg',
+		version: '9.5',
+		//connection: 'postgres://localhost/plateforme_db',
+		connection: {
+			host: 'localhost',
+			user: 'khaled',
+			password: 'khaled',
+			database: 'plateforme_db',
+			charset: 'utf8'
 		},
-		migrations : {
-			directory : __dirname + '/config/db/migrations'
+		migrations: {
+			directory: __dirname + '/config/db/migrations'
 		},
-		seeds      : {
-			directory : __dirname + '/config/db/seeds'
+		seeds: {
+			directory: __dirname + '/config/db/seeds'
 		}
 	},
-	production  : {
-		client     : 'mysql',
-		connection : {
-			host     : process.env.DB_HOST,
-			user     : process.env.DB_USER,
-			password : process.env.DB_PASSWORD,
-			database : process.env.DB_NAME,
-			charset  : 'utf8'
+	production: {
+		client: 'mysql',
+		connection: 'postgres://localhost/plateforme_db',
+		// connection: {
+		// 	host: process.env.DB_HOST,
+		// 	user: process.env.DB_USER,
+		// 	password: process.env.DB_PASSWORD,
+		// 	charset: 'utf8'
+		// }, 	database: process.env.DB_NAME,
+
+		migrations: {
+			directory: __dirname + '/db/migrations'
 		},
-		migrations : {
-			directory : __dirname + '/db/migrations'
-		},
-		seeds      : {
-			directory : __dirname + '/db/seeds/production'
+		seeds: {
+			directory: __dirname + '/db/seeds/production'
 		}
 	}
 };
